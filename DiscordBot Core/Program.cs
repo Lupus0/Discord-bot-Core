257c8fd4358afadc9d5cb59ddb0c01f572110a7b
Lupus0 <45178231+Lupus0@users.noreply.github.com>
2018-11-19 19:02:45 +0100

    first commit

---
+++ DiscordBot Core/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DiscordBot_Core
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
